b5d56e278483b87e745632af254f05fdce2d778f
Jonas Vanden Branden <vandenbranden.jonas@gmail.com>
2019-02-28 20:11:04 +0100

    Added score to slide

--- src/Slide.py
+++ src/Slide.py
@@ -15,3 +15,10 @@ class Slide:
             return str(self.photos[0].id)
         elif len(self.photos) == 2:
             return str(self.photos[0].id) + " " + str(self.photos[1].id)
+
+
+    def score(self,other_slide):
+        score1 = len(self.get_tags().intersection(other_slide.get_tags()))
+        score2 = len(self.get_tags().difference(other_slide.get_tags()))
+        score3 = len(self.get_tags().difference(other_slide.get_tags()))
+        return min([score1,score2,score3])
