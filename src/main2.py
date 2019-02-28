8d6593dd50a217523a974801e0d8a2a4fbb927a6
Unknown <robin.depauw5@gmail.com>
2019-02-28 23:21:29 +0100

    to late..

--- src/main2.py
+++ src/main2.py
@@ -1,6 +1,14 @@
+import random
+import time
+import tqdm as tqdm
+
+from Slide import Slide
 from SlideShow import SlideShow
 from InputParser import InputParser
 from VerticalCombinator import VerticalCombinator
+import itertools
+import matplotlib.pyplot as plt
+    
 
 def process_file(filename):
     parser = InputParser()
@@ -16,17 +24,41 @@ def write_output(slideshow, filename):
     outputfile = open("../output/" + filename + ".out", 'w')
     outputfile.write(out)
 
+def find_common(tags,photos):
+    set_copy = photos.copy()
+    for i in range(len(set_copy)):
+        candidate = set_copy.pop()
+        if len(tags.intersection(candidate.get_tags())) > 0:
+            return candidate
+
+    return None
+
+
+
 if __name__ == "__main__":
 
     files = ["a", "b", "c", "d", "e"]
-    files = ["e"]
+    files = ["b"]
 
     for f in files:
         print("starting file: "+f)
         photos = process_file(f)[1]
-        verts = set(filter(lambda x: x.orientation == "V",photos))
-        combinator = VerticalCombinator(verts)
-        combinator.plot_dist(verts)
-        combinator.plot_dist(combinator.random())
-        combinator.plot_dist(combinator.order_big_with_small())
-        combinator.plot_dist(combinator.max_union_tags_random(100))
+        list_result = []
+        selected = photos.pop()
+        list_result.append(Slide([selected]))
+        for i in range(len(photos)):
+            print(i)
+            next = find_common(selected.get_tags(),photos)
+            if next is not None:
+                list_result.append(Slide([next]))
+                selected = next
+                photos.remove(next)
+            else:
+                next = photos.pop()
+                list_result.append(Slide([next]))
+                selected = next
+        slideshow = SlideShow()
+        slideshow.set_slide_list(list_result)
+        write_output(slideshow, f)
+
+
