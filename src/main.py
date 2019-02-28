f725c8afcf27e31a3de5d1952df13b8055032744
Jonas Vanden Branden <vandenbranden.jonas@gmail.com>
2019-02-28 19:57:16 +0100

    MVP

--- src/main.py
+++ src/main.py
@@ -7,18 +7,20 @@ def process_file(filename):
     return parser.parse("../input/" + filename + ".in")
 
 def makeSlides(slideshow, photos):
-    cur_photo = get_next_photo(photos, {})
+    vertical_buffer = None
 
-    slideshow.add_slide(cur_photo)
     for i in range(len(photos)):
-        print("Adding photo")
-        cur_photo = get_next_photo(photos, cur_photo.tags)
-        slideshow.add_slide(cur_photo)
-    
+        cur_photo = photos.pop()
+        if(cur_photo.orientation == "H"):
+            slideshow.add_slide(cur_photo)
+        else:
+            if vertical_buffer is None:
+                vertical_buffer = cur_photo
+            else:
+                slideshow.add_doubleslide(vertical_buffer, cur_photo)
+                vertical_buffer = None
+  
     return slideshow
-
-def get_next_photo(photoset, tagset):
-    return photoset.pop()
 
 
 def write_output(slideshow, filename):
@@ -36,4 +38,5 @@ if __name__ == "__main__":
         photos = process_file(f)[1]
         slideshow = SlideShow()
         slideshow = makeSlides(slideshow, photos)
+        slideshow.optimize()
         write_output(slideshow, f)
