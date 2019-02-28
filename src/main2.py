f80fb6908f11bf1347f535ef8d6cc41b71892ab5
Unknown <robin.depauw5@gmail.com>
2019-02-28 20:18:50 +0100

    commit

---
+++ src/main2.py
@@ -0,0 +1,36 @@
+from SlideShow import SlideShow
+
+def makeSlides(slideshow, photos):
+    vertical_buffer = None
+
+    for i in range(len(photos)):
+        cur_photo = photos.pop()
+        if(cur_photo.orientation == "H"):
+            slideshow.add_slide(cur_photo)
+        else:
+            if vertical_buffer is None:
+                vertical_buffer = cur_photo
+            else:
+                slideshow.add_doubleslide(vertical_buffer, cur_photo)
+                vertical_buffer = None
+  
+    return slideshow
+
+
+def write_output(slideshow, filename):
+    out = slideshow.get_output()
+    outputfile = open("../output/" + filename + ".out", 'w')
+    outputfile.write(out)
+
+if __name__ == "__main__":
+
+    files = ["a", "b", "c", "d", "e"]
+    #files = ["a"]
+
+    for f in files:
+        print("starting file: "+f)
+        photos = process_file(f)[1]
+        slideshow = SlideShow()
+        slideshow = makeSlides(slideshow, photos)
+        slideshow.optimize()
+        write_output(slideshow, f)
