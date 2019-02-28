8463629325e4d1b39f69ecee44e4431a6aa0a5b1
Unknown <robin.depauw5@gmail.com>
2019-02-28 19:06:13 +0100

    photo class + input parser

--- src/InputParser.py
+++ src/InputParser.py
@@ -1,27 +1,29 @@
+from Photo import Photo
+
 class InputParser:
-    def __init__(self):
-        self.ridelist = list()
-        self.carlist = list()
 
-
+    class Photo:
+        def __init__(self,id,orientation,n_tags,tags):
+            self.id = id
+            self.orientation = orientation
+            self.n_tags = n_tags
+            self.tags = tags
 
     def parse(self,filename):
         inputfile = open(filename, 'r')
         firstline = inputfile.readline()
-        param = firstline[:-1].split(' ')
-        num_rows, num_cols, num_vehicles, num_rides, bonus, simsteps = int(param[0]), int(param[1]), int(param[2]), int(param[3]), int(param[4]), int(param[5])
+        n_photos = int(firstline)
 
-        for id in range(num_rides):
-            rideline = inputfile.readline()
-            param = rideline[:-1].split(' ')
-            row, col, fin_row, fin_col, start, finish = int(param[0]), int(param[1]), int(param[2]), int(param[3]), int(param[4]), int(param[5])
-            start_location = Location(row, col)
-            finish_location = Location(fin_row, fin_col)
-            ride = Ride(id, start_location, finish_location, start, finish)
-            self.ridelist.append(ride)
+        photo_set = set()
 
-        for car_id in range(num_vehicles):
-            car = Car(car_id)
-            self.carlist.append(car)
+        for id in range(n_photos):
+            photoline = inputfile.readline()
+            param = photoline[:-1].split(' ')
+            orientation, n_tags, tags = str(param[0]), int(param[1]), param[2:]
+            print(orientation)
+            print(n_tags)
+            print(tags)
+            photo = Photo(id,orientation,n_tags,tags)
+            photo_set.add(photo)
 
-        return self.ridelist, bonus, simsteps, num_rows, num_cols, self.carlist
+        return n_photos, photo_set
---
+++ src/Photo.py
@@ -0,0 +1,6 @@
+class Photo:
+    def __init__(self, id, orientation, n_tags, tags):
+        self.id = id
+        self.orientation = orientation
+        self.n_tags = n_tags
+        self.tags = tags
