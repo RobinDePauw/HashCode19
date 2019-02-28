93759e2fd7d607482bc34b91fdea119d4c20e30a
Matthias <mat.boen@gmail.com>
2019-02-28 19:28:11 +0100

    first main

--- src/main.py
+++ src/main.py
@@ -1,13 +1,27 @@
 from InputParser import InputParser
-from CommandCenter import CommandCenter
+from SlideShow import SlideShow
+
 
 def process_file(filename):
     parser = InputParser()
-    parsed input = parser.parse("../input/" + filename + ".in")
+    return parser.parse("../input/" + filename + ".in")
 
-    output = None
-    outputfile = open("../output/" + filename + ".out", 'w')
-    outputfile.write(output)
+def makeSlides(slideshow, photos):
+    cur_photo = get_next_photo(photos, {})
+
+    slideshow.add_slide(cur_photo)
+    for i in range(len(photos)):
+        print("Adding photo")
+        cur_photo = get_next_photo(photos, cur_photo.tags)
+        slideshow.add_slide(cur_photo)
+
+def get_next_photo(photoset, tagset):
+    return photoset.pop()
+
+
+   # output = None
+  #  outputfile = open("../output/" + filename + ".out", 'w')
+   # outputfile.write(output)
 
 if __name__ == "__main__":
 
@@ -15,4 +29,6 @@ if __name__ == "__main__":
 
     for file in files:
         print("starting file: "+file)
-        process_file(file)
+        photos = process_file(file)[1]
+        slideshow = SlideShow()
+        makeSlides(slideshow, photos)
