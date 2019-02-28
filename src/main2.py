d5f10bec3caca010ffdb79f133dd1d40b7b01298
Unknown <robin.depauw5@gmail.com>
2019-02-28 21:03:16 +0100

    vert combinator

--- src/main2.py
+++ src/main2.py
@@ -1,20 +1,14 @@
 from SlideShow import SlideShow
+from InputParser import InputParser
+from VerticalCombinator import VerticalCombinator
+
+def process_file(filename):
+    parser = InputParser()
+    return parser.parse("../input/" + filename + ".in")
+
 
 def makeSlides(slideshow, photos):
-    vertical_buffer = None
-
-    for i in range(len(photos)):
-        cur_photo = photos.pop()
-        if(cur_photo.orientation == "H"):
-            slideshow.add_slide(cur_photo)
-        else:
-            if vertical_buffer is None:
-                vertical_buffer = cur_photo
-            else:
-                slideshow.add_doubleslide(vertical_buffer, cur_photo)
-                vertical_buffer = None
-  
-    return slideshow
+    pass
 
 
 def write_output(slideshow, filename):
@@ -30,7 +24,6 @@ if __name__ == "__main__":
     for f in files:
         print("starting file: "+f)
         photos = process_file(f)[1]
-        slideshow = SlideShow()
-        slideshow = makeSlides(slideshow, photos)
-        slideshow.optimize()
-        write_output(slideshow, f)
+        verts = set(filter(lambda x: x.orientation == "V",photos))
+        combinator = VerticalCombinator(verts)
+        combinator.plot_dist()
